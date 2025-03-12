b8814d966d77db9ca7c1ce6374380acab1eb30bc
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-03-13 03:03:46 +1100

    reset

--- dataview/app/layout.tsx
+++ dataview/app/layout.tsx
@@ -2,10 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-// side bar
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/sidebar/app-sidebar"
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -31,13 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SidebarProvider>
-          <AppSidebar />
-          <main>
-            <SidebarTrigger />
-            {children}
-          </main>
-        </SidebarProvider>
+      {children}
       </body>
     </html>
   );
