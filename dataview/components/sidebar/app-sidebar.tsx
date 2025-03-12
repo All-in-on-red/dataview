f4da43ac653b4b7caf6bb71627f63561e53f8cea
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-03-13 03:43:43 +1100

    collapsible sidebar to icon mode

--- dataview/components/sidebar/app-sidebar.tsx
+++ dataview/components/sidebar/app-sidebar.tsx
@@ -42,21 +42,21 @@ const items = [
 
 export function AppSidebar() {
   return (
-    <Sidebar>
+    <Sidebar collapsible="icon">
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <a href={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </a>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
+                      <SidebarMenuItem key={item.title}>
+                          <SidebarMenuButton asChild>
+                              <a href={item.url}>
+                                  <item.icon/>
+                                  <span>{item.title}</span>
+                              </a>
+                          </SidebarMenuButton>
+                      </SidebarMenuItem>
               ))}
             </SidebarMenu>
           </SidebarGroupContent>
