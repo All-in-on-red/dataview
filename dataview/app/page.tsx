bab65c65978ec17d5ce305751d5d485552ada09a
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-03-13 04:16:41 +1100

    custom sidebar trigger for customization purpose

--- dataview/app/page.tsx
+++ dataview/app/page.tsx
@@ -1,6 +1,6 @@
 // side bar
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { AppSidebar } from "@/components/sidebar/app-sidebar"
+import { SidebarProvider} from "@/components/ui/sidebar"
+import { AppSidebar, SidebarTrigger} from "@/components/sidebar/app-sidebar"
 
 export default function Home() {
   return (
--- dataview/components/sidebar/app-sidebar.tsx
+++ dataview/components/sidebar/app-sidebar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
 
 import {
@@ -63,5 +64,40 @@ export function AppSidebar() {
         </SidebarGroup>
       </SidebarContent>
     </Sidebar>
+  )
+}
+
+import { cn } from "@/lib/utils"
+import { Button } from "../ui/button"
+import { useSidebar } from "../ui/sidebar"
+import { PanelLeftIcon } from "lucide-react"
+
+export function SidebarTrigger({
+  className,
+  onClick,
+  children,
+  ...props
+}: React.ComponentProps<typeof Button>) {
+  const { toggleSidebar } = useSidebar()
+
+  return (
+    <Button
+      data-sidebar="trigger"
+      data-slot="sidebar-trigger"
+      variant="ghost"
+      size="icon"
+      className={cn("h-7 w-auto ml-5 p-2", className)}
+      onClick={(event) => {
+        onClick?.(event)
+        toggleSidebar()
+      }}
+      {...props}
+    >
+      <PanelLeftIcon />
+      <span className="sr-only">Toggle Sidebar</span>
+      <span>
+        Hello world
+      </span>
+    </Button>
   )
 }
