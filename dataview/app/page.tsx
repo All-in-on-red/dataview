fd51f08b7658a9ad7fc89683206e53a793494165
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-03-13 03:05:23 +1100

    moved sidebar into the page because i dont think this apps has multiple pages

--- dataview/app/page.tsx
+++ dataview/app/page.tsx
@@ -1,2 +1,14 @@
+// side bar
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/sidebar/app-sidebar"
+
 export default function Home() {
+  return (
+  <SidebarProvider>
+    <AppSidebar />
+    <main>
+      <SidebarTrigger />
+    </main>
+  </SidebarProvider>
+  )
 }
