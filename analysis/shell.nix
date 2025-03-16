31187beb232fcb3986255425b549bae30783aff0
Cofee Coder <41608260+CoffeeCoder1015@users.noreply.github.com>
2025-03-16 18:17:22 +1100

    nix-shell updates

--- analysis/shell.nix
+++ analysis/shell.nix
@@ -1,7 +1,7 @@
 let
     pkgs = import <nixpkgs> {config = {}; overlays=[];};
 in
-pkgs.mkShellNoCC{
+pkgs.mkShell{
   name = "py2wasm-shell";
   buildInputs = [
     pkgs.python311
@@ -14,8 +14,8 @@ pkgs.mkShellNoCC{
     if [ ! -d .venv ]; then
       echo "Creating virtual environment..."
       python -m venv .venv
-      .venv/bin/pip install --upgrade pip
-      .venv/bin/pip install py2wasm
+      .venv/bin/pip3 install --upgrade pip
+      .venv/bin/pip3 install py2wasm
     fi
     echo "Activating virtual environment..."
     source .venv/bin/activate
